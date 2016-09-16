600834962c09a76662e476290d4b03d9661d77b7
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2016-09-16 09:05:24 +0200

    item-field editor helper

---
+++ itemField/editor.js
@@ -0,0 +1,27 @@
+import { createElement as el } from 'react'
+import { observer } from 'mobservable-react'
+import get from 'lodash/get'
+
+export default function(renderer) {
+  return function (collections, collection, itemId, fieldArg) {
+    var model = typeof collection === 'string' ? collections[collection].model : collection
+
+    return observer(function () {
+      if (!itemId) return null
+
+      var item = model.get(itemId)
+      if (!item.loaded) return el('span', null, '...')
+
+      return el(renderer({
+        getValue: () => fieldArg.path ? get(item.value, fieldArg.path) : item.value,
+        setValue: (value) => {
+          let patch = value
+          if (fieldArg.path) {
+            patch = {[fieldArg.path]: value}
+          }
+          return model.patch(itemId, patch)
+        },
+      }))
+    })
+  }
+}
