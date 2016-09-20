119240baecffa86d4fd2ec4c9c1f62c1446c17ee
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2016-09-20 13:08:12 +0200

    fixed itemField/editor

--- itemField/editor.js
+++ itemField/editor.js
@@ -2,26 +2,37 @@ import { createElement as el } from 'react'
 import { observer } from 'mobservable-react'
 import get from 'lodash/get'
 
-export default function(renderer) {
+export default function(makeRenderer) {
   return function (collections, collection, itemId, fieldArg) {
     var model = typeof collection === 'string' ? collections[collection].model : collection
+
+    const renderer = makeRenderer({
+      getValue: () => {
+        var item = model.get(itemId)
+        if (fieldArg.path) {
+          return get(item.value, fieldArg.path)
+        } else {
+          return item.value  
+        }
+      },
+      setValue: (value) => {
+        let patch = value
+        if (fieldArg.path) {
+          patch = {[fieldArg.path]: value}
+        }
+        return model.patch(itemId, patch)
+      },
+    })
 
     return observer(function () {
       if (!itemId) return null
 
       var item = model.get(itemId)
-      if (!item.loaded) return el('span', null, '...')
-
-      return el(renderer({
-        getValue: () => fieldArg.path ? get(item.value, fieldArg.path) : item.value,
-        setValue: (value) => {
-          let patch = value
-          if (fieldArg.path) {
-            patch = {[fieldArg.path]: value}
-          }
-          return model.patch(itemId, patch)
-        },
-      }))
+      if (item.loaded) {
+        return el(renderer)
+      } else {
+        return el('span', null, '...')
+      }
     })
   }
 }
