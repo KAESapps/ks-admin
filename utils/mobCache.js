6d913523687da99b1bf56931c047e15dff90f136
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2016-10-13 16:33:26 +0200

    add onDisposed in mobCache

--- utils/mobCache.js
+++ utils/mobCache.js
@@ -1,15 +1,15 @@
-export default create => {
+export default (create, onDisposed) => {
   var cache = new Map()
   var get = key => {
     if (cache.has(key)) return cache.get(key)()
     var obs = create(key)
     // console.log('obs created', key)
     cache.set(key, obs)
-    // TODO: supprimer les entrées du cache quand elles ne sont plus utilisées
-    // obs.$mobservable.onceSleep(() => {
-    //   cache.delete(key)
-    //   // console.log('obs disposed', key)
-    // })
+    obs.$mobservable && obs.$mobservable.onceSleep && obs.$mobservable.onceSleep(() => {
+      onDisposed && onDisposed(key)
+      cache.delete(key)
+      // console.log('obs disposed', key)
+    })
     return obs()
   }
   get.cache = cache
