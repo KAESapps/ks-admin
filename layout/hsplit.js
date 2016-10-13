7ef06f5835b91fc9851d4db1d522b523c67c5226
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2016-10-13 16:33:49 +0200

    allow to reuse only UI container of hsplit

--- layout/hsplit.js
+++ layout/hsplit.js
@@ -3,19 +3,21 @@ import { observer } from 'mobservable-react'
 
 import { Box } from './flex'
 
+export const container = ({leftView, rightView}) => () => {
+  return el(Box, { style: { flexDirection: 'row' } },
+    el(Box, { style: { overflow: 'auto', flexShrink: 0 } },
+      el(leftView)
+    ),
+    el(Box, { style: { overflow: 'auto', flexShrink: 0 } },
+      el(rightView)
+    )
+  )
+}
+
 export default ({ leftView: makeLeftView, rightView: makeRightView }) => {
   return function() {
     const leftView = makeLeftView.apply(makeLeftView, arguments)
     const rightView = makeRightView.apply(makeRightView, arguments)
-    return observer(function() {
-      return el(Box, { style: { flexDirection: 'row' }},
-        el(Box, { style: { overflow: 'auto', flexShrink: 0 } },
-          el(leftView)
-        ),
-        el(Box, { style: { overflow: 'auto', flexShrink: 0 } },
-          el(rightView)
-        )
-      )
-    })
+    return observer(container({leftView, rightView}))
   }
 }
