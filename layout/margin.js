e28635c8a842b1b6119f1d5f2050fd795efe592a
Quentin Vuilliot <quentin.vuilliot@kaes.fr>
2016-12-02 12:42:55 +0100

    Customizable margin

--- layout/margin.js
+++ layout/margin.js
@@ -1,10 +1,24 @@
 import { createElement as el } from 'react'
+import assertArgs from 'assert-args'
+import defaults from 'lodash/defaults'
 
-export default (makeView) => {
+export default function () {
+  const { opts, view } = assertArgs(arguments, {
+    '[opts]': 'object',
+    view: 'function',
+  })
+
+  const margins = defaults(opts, { l: '1em', r: '1em', t: '1em', b: '1em' })
+
   return function() {
-    const view = makeView.apply(makeView, arguments)
+    const reactCmp = view.apply(view, arguments)
     return function() {
-      return el('div', { style: { padding: '1em' } }, el(view))
+      return el('div', { style: {
+        paddingLeft: margins.l,
+        paddingRight: margins.r,
+        paddingTop: margins.t,
+        paddingBottom: margins.b,
+      } }, el(reactCmp))
     }
   }
 }
